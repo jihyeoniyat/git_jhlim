dbd6dd07f37bf4ab969dd2682d39b8a4ffc1a141
jihyeoniyat <ljih0371@naver.com>
2023-11-14 17:21:47 +0900

    kpx_fault api 호출 테스트, 전처리

--- 2. 프로젝트/3. 23_MLOps PV 발전량 예측 모델 적용/2. Fault 알람 데이터/1. 호출 Test/kpx_alarm_test.py
+++ 2. 프로젝트/3. 23_MLOps PV 발전량 예측 모델 적용/2. Fault 알람 데이터/1. 호출 Test/kpx_alarm_test.py
@@ -4,27 +4,23 @@ import pandas as pd
 from pytz import timezone
 from azure.storage.blob import BlobServiceClient
 from datetime import datetime, timedelta
-import argparse
 import os
-import xml.etree.ElementTree as ET
 import json
 
-
-# 세션 생성
+## Login 및 세션 생성
 session = requests.Session()
 
-## Login
 url_login = 'https://essonm.ls-electric.com/api/login'
 m = MultipartEncoder(fields = {'username': 'majin', 'password': '12345'}, encoding='utf-8')
-
 response_login = session.post(url_login, data=m, headers={'Content-Type': m.content_type})
 
 response_login.raise_for_status()  # 오류 발생 시 예외 처리
 
 
+## api 데이터 호출
 api_url = 'https://essonm.ls-electric.com/api/alarms'
 
-# # API 요청을 위한 데이터
+## API 요청을 위한 데이터 - PV(PCU)
 # data = {
 #     'deviceIds': [
 #         "YA.PV01.PCU01",
@@ -74,25 +70,60 @@ api_url = 'https://essonm.ls-electric.com/api/alarms'
 #         "YA.PV45.PCU01"
 #     ],
 #     'types': ['StatusInfo'],
-#     'unresolved': "true",
+#     'unresolved': False,
 #     'startDate': '2023-03-23',
 #     'tagIds': ['DIG_UNIT1_FLT_STOP_STTS']
 # }
 
-# API 요청을 위한 데이터
+# API 요청을 위한 데이터 - ESS(PCS)
 data = {
     'deviceIds': [
         "YA.ESS01.PCS01",
         "YA.ESS01.PCS02",        
         "YA.ESS01.PCS03",
         "YA.ESS01.PCS04",
+        "YA.ESS01.PCS05",
+        "YA.ESS01.PCS06",        
+        "YA.ESS01.PCS07",
+        "YA.ESS01.PCS08",
+        "YA.ESS01.PCS09",
+        "YA.ESS01.PCS10",
+        "YA.ESS01.PCS11",
+        "YA.ESS01.PCS12",
+        "YA.ESS01.PCS13",
+        "YA.ESS01.PCS14",
+        "YA.ESS01.PCS15",
+        "YA.ESS01.PCS16",
+        "YA.ESS01.PCS17",
+        "YA.ESS01.PCS18",
+        "YA.ESS01.PCS19",
+        "YA.ESS01.PCS20",
+        "YA.ESS01.PCS21",
+        "YA.ESS01.PCS22",
+        "YA.ESS01.PCS23",
+        "YA.ESS01.PCS24",
+        "YA.ESS01.PCS25",
+        "YA.ESS01.PCS26",
+        "YA.ESS01.PCS27",
+        "YA.ESS01.PCS28",
+        "YA.ESS01.PCS29",
+        "YA.ESS01.PCS30",
+        "YA.ESS01.PCS31",
+        "YA.ESS01.PCS32",
+        "YA.ESS01.PCS33",
+        "YA.ESS01.PCS34",
+        "YA.ESS01.PCS35",
+        "YA.ESS01.PCS36",
+        "YA.ESS01.PCS37",
+        "YA.ESS01.PCS38",
+        "YA.ESS01.PCS39"
     ],
     'types': ['Fault','StatusInfo'],
-    'unresolved': False,
-    'startDate': '2023-10-23',
+    'unresolved': True,
+    'startDate': '2023-08-29',
+    'endDate' : '2023-09-07',
     'tagIds': ['DIG_POFF_STTS','DIG_FLT_STTS']
 }
-
 
 
 # 요청 보내기
@@ -106,46 +137,43 @@ except requests.exceptions.RequestException as err:
     print(f'요청 오류: {err}')
 
 
-
 # JSON 응답 문자열을 파이썬 객체로 변환
 result_data = response.json()
-
-# 데이터프레임 생성
-df = pd.json_normalize(result_data)
-# 'statusInfoList'의 값들을 별도의 데이터프레임으로 만들기
-status_info_list = []
-for entry in result_data:
-    for status_info in entry.get('statusInfoList', []):
-        entry_dict = {
-            'displayDisable': entry.get('displayDisable'),
-            'statusInfoList_id': status_info.get('id'),
-            'statusInfoList_value': status_info.get('value')
-        }
-        status_info_list.append(entry_dict)
-
-status_info_df = pd.DataFrame(status_info_list)
-
-# 'displayDisable' 컬럼을 기준으로 두 데이터프레임을 병합
-merged_df = pd.merge(df, status_info_df, on='displayDisable', how='left')
-merged_df = merged_df.drop("statusInfoList", axis=1)
-
-merged_df.to_csv('/Users/jihyeon/Desktop/test2.csv', index=False)
+print(result_data[0].get("alarmStatusList"))
 
 
-# ## JSON 파싱 -> list
-# json_data = response.json()
+# "alarmMessageId"가 "DIG_FLT_STTS+0"인 딕셔너리만 필터링
+# filtered_list = [entry for entry in result_data 
+#                  if "alarmStatusList" in entry and any(status.get("alarmMessageId") == "DIG_FLT_STTS+0" for status in entry.get("alarmStatusList", []))]
 
-# # 빈 리스트 생성
-# data_list = []
-
-
-# # JSON 데이터를 리스트에 추가
-# for item in json_data['statusInfoList']:
-#     data_list.append(item)
-
-# # 리스트를 데이터프레임으로 변환
-# df = pd.DataFrame(data_list)
-# print(df)
+# "statusInfoList"가 "DIG_POFF_STTS"인 딕셔너리만 필터링
+# filtered_list = [entry for entry in result_data 
+#                  if "statusInfoList" in entry and any(status.get("id") == "DIG_POFF_STTS" for status in entry.get("statusInfoList", []))]
 
 
-# display(response)
+# # 데이터프레임 생성
+# df = pd.json_normalize(filtered_list)
+
+
+# # 'statusInfoList'의 값들을 별도의 데이터프레임으로 만들기
+# status_info_list = []
+# for entry in result_data:
+#     for status_info in entry.get('statusInfoList', []):
+#         entry_dict = {
+#             'displayDisable': entry.get('displayDisable'),
+#             'statusInfoList_id': status_info.get('id'),
+#             'statusInfoList_value': status_info.get('value')
+#         }
+#         status_info_list.append(entry_dict)
+
+# status_info_df = pd.DataFrame(status_info_list)
+
+# # 'displayDisable' 컬럼을 기준으로 두 데이터프레임을 병합
+# merged_df = pd.merge(df, status_info_df, on='displayDisable', how='left')
+# merged_df = merged_df.drop("statusInfoList", axis=1)
+
+
+
+# merged_df.to_csv('/Users/jihyeon/Desktop/ESS_Test_resolved.csv', index=False)
+
+# df.to_json('/Users/jihyeon/Desktop/ESS_filter_Resolved.json', orient='records', lines=True)
